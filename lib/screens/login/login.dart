744c4b611ea641196f6a697b79c9f12ff5689c41
meghanamu <meghanamu@gmail.com>
2021-07-28 15:31:32 -0700

    pages added

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_template_project/helpers/style.dart';
-import 'package:flutter_template_project/screens/home/home.dart';
+import 'package:flutter_template_project/screens/buyerHome/home.dart';
 import 'package:flutter_template_project/screens/registration/registration.dart';
 import 'package:flutter_template_project/widgets/custom_button.dart';
 import 'package:flutter_template_project/widgets/custom_text.dart';
