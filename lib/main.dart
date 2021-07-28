4ea5e7e32be464f9b7aede134ec1023d600b5cdb
meghanamu <meghanamu@gmail.com>
2021-07-28 14:58:55 -0700

    change project title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'testCommit',
       theme: ThemeData(
   
         primarySwatch: Colors.blue,
