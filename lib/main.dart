a9c454583305f95260b1e02ebf54560ec1d24525
tridentlmao <shashankch008@gmail.com>
2021-07-28 18:08:21 -0400

    monke

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'testCommit',
+      title: 'monkeman69420',
       theme: ThemeData(
   
         primarySwatch: Colors.blue,
