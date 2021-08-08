ca0c559915514d12fed38ec9a3eefed73e2cce3d
Ashcrafter55 <aashritmarri@gmail.com>
2021-08-08 15:52:40 -0400

    Edited seller profile page

--- lib/screens/sellerHome/SellerProfile.dart
+++ lib/screens/sellerHome/SellerProfile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_template_project/helpers/style.dart';
+import 'package:flutter_template_project/helpers/globalVar.dart';
 
 class SellProfile extends StatefulWidget {
   SellProfile({Key key}) : super(key: key);
@@ -19,52 +19,73 @@ class _SellProfileState extends State<SellProfile> {
                 margin: EdgeInsets.only(top: 150),
                 child: CircleAvatar(
                   radius: 40,
-                  backgroundColor: Colors.purple,
-                  child: const Text("SB", style: TextStyle(
-                      fontSize: 20
-                  ),),
+                  backgroundColor: Colors.white,
+                  child: Image.asset("images/Chef.png")
                 ) ,
               ),
               Row(mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
                     margin: EdgeInsets.only(top:15),
-                    child: Text("Simon's Bakery", style: TextStyle(
+                    child: Text("${restaurantName.text}", style: TextStyle(
                         fontSize: 20
                     ),),
                   )
                 ],
               ),
-              Container(
-                padding: EdgeInsets.only(left:20),
-                margin: EdgeInsets.only(top:20),
-                child: Card(
-                  color: Color(0xFF8fdeebff) ,
-                  child: ListTile(
-                    leading: Icon(Icons.edit),
-                    title: Text("Restaruant Name"),
+              Center(
+                child: Container(
+                  width: 300,
+                  padding: EdgeInsets.only(left:20),
+                  margin: EdgeInsets.only(top:20),
+                  child: Card(
+                    color: Color(0xFF8fdeebff) ,
+                    child: ListTile(
+                      leading: Icon(Icons.edit),
+                      title: Text("Restaruant Name"),
+                    ),
                   ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.only(left:20),
-                margin: EdgeInsets.only(top:20),
-                child: Card(
-                  color: Color(0xFF8fdeebff) ,
-                  child: ListTile(
-                    leading: Icon(Icons.edit),
-                    title: Text("Location"),
+              Center(
+                child: Container(
+                  width: 300,
+                  padding: EdgeInsets.only(left:20),
+                  margin: EdgeInsets.only(top:20),
+                  child: Card(
+                    color: Color(0xFF8fdeebff) ,
+                    child: ListTile(
+                      leading: Icon(Icons.edit),
+                      title: Text("Description"),
+                    ),
                   ),
                 ),
               ),
-              Container(
-                padding: EdgeInsets.only(left:20),
-                margin: EdgeInsets.only(top:20),
-                child: Card(
-                  color: Color(0xFF8fdeebff) ,
-                  child: ListTile(
-                    leading: Icon(Icons.edit),
-                    title: Text("Menu"),
+              Center(
+                child: Container(
+                  width: 300,
+                  padding: EdgeInsets.only(left:20),
+                  margin: EdgeInsets.only(top:20),
+                  child: Card(
+                    color: Color(0xFF8fdeebff) ,
+                    child: ListTile(
+                      leading: Icon(Icons.edit),
+                      title: Text("Location"),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: Container(
+                  width: 300,
+                  padding: EdgeInsets.only(left:20),
+                  margin: EdgeInsets.only(top:20),
+                  child: Card(
+                    color: Color(0xFF8fdeebff) ,
+                    child: ListTile(
+                      leading: Icon(Icons.edit),
+                      title: Text("Menu"),
+                    ),
                   ),
                 ),
               ),
